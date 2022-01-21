8df6ac073f494983d0db9ae3cf12f5f3a7f475ff
Vonfagelsang <89455450+Vonfagelsang@users.noreply.github.com>
2022-01-21 15:44:34 +0100

    Add files via upload

---
+++ Fordon.cs
@@ -0,0 +1,14 @@
+class Fordon{
+    public string tillvärkare;
+    public string modell;
+    public string årsmodell;
+    public int vikt;
+
+    public Fordon(string t, string m, string å, int v){
+        tillvärkare = t;
+        modell = m;
+        årsmodell = å;
+        vikt = v;
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program {
+
+    public static void Main () 
+    {
+        List<Fordon> Fordonslista = new List<Fordon>();
+
+        Fordon bil = new Fordon("Fård", "grön", "V3", 1500);
+        Fordonslista.Add(bil);
+        Fordonslista.Add(new Fordon("Volvo", "gul", "V70", 2000));
+
+        foreach (Fordon element in Fordonslista)
+        {
+            Console.WriteLine(element.tillvärkare);
+            Console.WriteLine(element.årsmodell);
+            Console.WriteLine(element.modell);
+            Console.WriteLine(element.vikt);
+        }
+    }
+}
